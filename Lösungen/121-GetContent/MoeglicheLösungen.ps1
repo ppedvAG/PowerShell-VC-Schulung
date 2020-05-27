1a15268ccd7b386aed5974ec35b8dd1256a52bd9
Stefan Ober <StefanO@ppedv.de>
2020-05-27 15:21:08 +0200

    Lösung zu Get Content Übung. FolienNr 121

---
+++ Lösungen/121-GetContent/MoeglicheLösungen.ps1
@@ -0,0 +1,8 @@
+﻿#Einfache Lösung Laudauer: TotalMilliseconds : 8,8894
+Get-Content -Path C:\Testfiles\MyServices.txt | Get-Service
+
+#Effizientere Lösung Laufdauer: TotalMilliseconds : 7,6507
+Get-Service -DisplayName (Get-Content -Path C:\Testfiles\MyServices.txt)
+
+#cmdlet zum Messen der Ausführungszeit eines cmdlet oder Skriptes
+Measure-Command {<#Befehl / Skript #>}

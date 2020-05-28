a71dfe6497dbfa9e4740fc4d7a1f82603be4ac18
Stefan Ober <StefanO@ppedv.de>
2020-05-28 11:20:10 +0200

    Ordner Beispiele erstellt und Datei Schleifenexamples dahin verschoben und neue Datei mit Verzweigungs Beispielen angelegt

---
+++ Beispiele/Verzweigungen.ps1
@@ -0,0 +1,22 @@
+﻿
+if(2 -eq 3)
+{
+    Write-Host "Something within the Matrix is broken" -BackgroundColor Black -ForegroundColor Green
+}
+elseif(1 -gt 2)
+{
+    Write-Host "Something within the Matrix is broken" -BackgroundColor Black -ForegroundColor Green
+}
+else
+{
+    Write-Host "Everything is fine"
+}
+
+$a = 2
+switch($a)
+{
+    1 {Write-Host -ForegroundColor Red "Ausgabe 1"}
+    2 {Write-Host -ForegroundColor Green "Ausgabe 2"}
+    3 {Write-Host -ForegroundColor White "Ausgabe 3"}
+    default {Write-Host -ForegroundColor Red "Falsche eingabe"}
+}

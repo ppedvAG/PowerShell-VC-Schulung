e0955e84fb0f60cbc128a21933a6999c2a04f85c
Stefan Ober <StefanO@ppedv.de>
2020-05-28 12:24:00 +0200

    Moegliche Loesung zum import von Usern via CSV

---
+++ Lösungen/183-ImportUsersFromCSV/MoeglicheLoesung.ps1
@@ -0,0 +1,12 @@
+﻿#einfacher Import , User bleiben aber deaktiviert weil Passwort ein String ist aber ein SecureString erforderlich ist
+Import-Csv -Path C:\Testfiles\MyNewAdUsers1.csv | New-ADUser
+
+#Import mit manuellem Mapping der Properties zu Parameter um beim AccountPassword  eine konvertierung zu Securestring zu erreichen. 
+Import-Csv -Path C:\Testfiles\MyNewAdUsers2.csv | ForEach-Object { New-ADUser -GivenName $PSItem.GivenName `
+                                                                              -Surname $PSItem.SurName `
+                                                                              -Name $PSItem.Name `
+                                                                              -SamAccountName $PSItem.SamAccountName `
+                                                                              -Department $PSItem.Department `
+                                                                              -Path $PSItem.Path `
+                                                                              -Enabled ([bool]$PSItem.Enabled) `
+                                                                              -AccountPassword (ConvertTo-SecureString $PSItem.Password -AsPlainText -Force) }

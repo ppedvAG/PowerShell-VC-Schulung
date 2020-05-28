e3e1a8da7addbcdb59b361d6a61258b8a9f7c88d
Stefan Ober <StefanO@ppedv.de>
2020-05-28 09:42:34 +0200

    Get-aduser Filter hinzugefügt

--- Lösungen/131-Filterung/MoeglicheLoesung.ps1
+++ Lösungen/131-Filterung/MoeglicheLoesung.ps1
@@ -1,2 +1,5 @@
 ﻿
 Get-Service | Where-Object {$PSItem.Name.Length -gt 15}
+
+#Wenn cmdlet eigene Filter haben dann diese verwenden anstatt von where-object
+Get-ADUser -Filter {GivenName -eq "Paul"}

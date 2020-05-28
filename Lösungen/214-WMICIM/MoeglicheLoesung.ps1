2637e435dade28111779c1509d6eb9dbc4a9e8b4
Stefan Ober <StefanO@ppedv.de>
2020-05-28 14:40:24 +0200

    Moegliche Loesungen zu WMI/CIM

---
+++ Lösungen/214-WMICIM/MoeglicheLoesung.ps1
@@ -0,0 +1,12 @@
+﻿#Aufgabe1
+Get-CimClass -Namespace root\cimv2
+#Alternativ
+Get-WmiObject -Namespace root\cimv2 -List
+
+#Aufgabe2
+Get-WmiObject -Class Win32_OperatingSystem
+Get-CimInstance -ClassName Win32_OperatingSystem
+
+#Aufgabe3
+Get-CimInstance -ClassName Win32_OperatingSystem | Format-List -Property *
+Get-CimInstance -ClassName Win32_OperatingSystem -Property *

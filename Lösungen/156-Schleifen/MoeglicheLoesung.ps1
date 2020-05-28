73468c8170ad92c68c7ea682ebe038d012ab19b4
Stefan Ober <StefanO@ppedv.de>
2020-05-28 10:40:08 +0200

    Moegliche Lösung zur Übung Schleifen hinzugefügt und Syntax für andere Schleifen

---
+++ Lösungen/156-Schleifen/MoeglicheLoesung.ps1
@@ -0,0 +1,19 @@
+﻿#Zählweise auf 2 erhöht
+for($i = 2;$i -le 10;$i = $i +2)
+{
+    $i
+}
+#einfache zählweise aber mit 2 multipliziert
+for($i = 1;$i -le 5;$i++)
+{
+    $i *2
+}
+
+#ganzzahliges Teilen (Modulo) durch 2 und bei REst 0 ausgabe
+for($i=1;$i -le 10; $i++)
+{
+    if($i%2 -eq 0)
+    {
+        $i
+    }
+}
---
+++ Lösungen/156-Schleifen/SchleifenExamples.ps1
@@ -0,0 +1,31 @@
+﻿#For Schleife
+#Zählervariable deklarieren ; Fortsetzungsbedingung ; Zählweise wichtig das ergebnis wieder in der Zähler variable zu speichern
+for($i = 1; $i -le 10; $i++)
+{
+    #SchleifenKörper
+}
+
+#ForeachSchleife
+
+Foreach($object in $ListOfObjects)
+{
+    #Do Something with $object
+}
+
+#While Schleife Kopfgeprüft
+While($true <# Prüfung #>)
+{
+    #Schleifenkörper
+}
+
+#do while fußgeprüft
+do
+{
+    #Schleifenkörper wird mindestens einmal durchlaufen
+}while(<# Bedingung solange true läuft weiter #>)
+
+#do until fußgeprüft
+do
+{
+    #schleifenkörper wird mindestens einmal durchlaufen
+}until(<# läuft weiter solange bedingung false ergbibt #>)

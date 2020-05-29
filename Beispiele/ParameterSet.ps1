5ff4a07ffde31c04843037d60e96b769187f60b0
Stefan Ober <StefanO@ppedv.de>
2020-05-29 12:56:36 +0200

    Beispiel für ParameterSet hinzugefügt

---
+++ Beispiele/ParameterSet.ps1
@@ -0,0 +1,14 @@
+﻿[cmdletBinding()]
+param(
+    [Parameter(ParameterSetName="Set1")]
+    [int]$start,
+
+    [Parameter(ParameterSetName="Set1")]
+    [int]$ende,
+
+    [ValidateRange(5,10)]
+    [Parameter(ParameterSetName="Set2")]
+    [int[]]$range
+
+
+)

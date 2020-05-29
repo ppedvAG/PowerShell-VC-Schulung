48c1a17a81a42c868ed3c74b9080f0ef6b90203d
Stefan Ober <StefanO@ppedv.de>
2020-05-29 09:17:57 +0200

    Skript erstellt mit Parametern

---
+++ Lösungen/230-Skripting/GetLogonLogoffEvents.ps1
@@ -0,0 +1,12 @@
+﻿[cmdletBinding()]
+Param(
+
+[string]$ComputerName = "localhost", 
+
+[Parameter(Mandatory=$true)]
+[int]$EventID,
+
+[int]$Newest = 5
+)
+
+Get-EventLog -LogName Security -ComputerName $ComputerName | Where-Object EventID -eq $EventID | Select-Object -First $Newest
